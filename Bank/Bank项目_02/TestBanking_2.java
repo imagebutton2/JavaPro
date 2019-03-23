3fb15bdc5d921efa1827599f9f3276430b66bce5
imagebutton2 <1046509851@qq.com>
2019-03-23 18:15:59 +0800

    扩展银行项目，添加一个 Customer 类。 Customer 类将包含一个 Account对象。 实验目的：使用引用类型的成员变量提 示： 1. 在banking包下的创建Customer类。该类必须实现上面的UML图表中的模型。 a. 声明三个私有对象属性：firstName、lastName 和 account。 b. 声明一个公有构造器，这个构造器带有两个代表对象属性的参数（f 和 l） c. 声明两个公有存取器来访问该对象属性，方法 getFirstName 和 getLastName返回相应的属性。 d. 声明 setAccount 方法来对 account 属性赋值。e. 声明 getAccount 方法以获取 account 属性。 2. 在 exercise2 主目录里，编译运行这个 TestBanking 程序。 应该看到如下输出结果: Creating the customer Jane Smith. Creating her account with a 500.00 balance. Withdraw 150.00 Deposit 22.50 Withdraw 47.62 Customer [Smith, Jane] has a balance of 324.

---
+++ Bank/Bank项目_02/TestBanking_2.java
@@ -0,0 +1,39 @@
+
+
+public class TestBanking_2 {
+
+	public static void main(String[] args) {
+		Customer customer;
+		// Account account;
+
+		// Create an account that can has a 500.00 balance.
+		System.out.println("Creating the customer Jane Smith.");
+		// code
+		customer = new Customer("Jane", "Smith");
+
+		System.out.println("Creating her account with a 500.00 balance.");
+		// code
+		// account = new Account(500);
+		// customer.setAccount(account);
+
+		customer.setAccount(new Account(500));
+
+		System.out.println("Withdraw 150.00");
+
+		// code
+		customer.getAccount().withdraw(150);
+
+		System.out.println("Deposit 22.50");
+		// code
+		customer.getAccount().deposit(22.50);
+
+		System.out.println("Withdraw 47.62");
+		// code
+		customer.getAccount().withdraw(47.62);
+
+		// Print out the final account balance
+		System.out.println("Customer [" + customer.getLastName() + ", "
+				+ customer.getFirstName() + "] has a balance of "
+				+ customer.getAccount().getBalance());
+	}
+}
